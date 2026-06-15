0e1e2aaa9cf0734ae09db9752e94f15174e70d1e
dhanesh2004 <ppdhanesh46@gmail.com>
2026-06-16 02:26:31 +0530

    Day 2 - user registration system

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def home(request):
-    return render(request,'accounts/home.html')
+    return render(request, 'accounts/home.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            print("FORM IS VALID")
+            form.save()
+            print("USER SAVED")
+            return redirect('home')
+
+        else:
+            print(form.errors)
+
+    else:
+        form = SignUpForm()
+
+    return render(
+        request,
+        'accounts/signup.html',
+        {'form': form}
+    )
